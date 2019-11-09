dca6e128a833d5d3c6aa3c6b9a7ffc8fc4870702
Mumtaj-123 <57558099+Mumtaj-123@users.noreply.github.com>
2019-11-09 22:20:15 +0530

    Add files via upload

---
+++ arithmetic_operation.py
@@ -0,0 +1,16 @@
+#wap to perform all arithemetic operation.
+
+a=int(input("Enter your desired number:"))
+
+b=int(input("\nEnter your another number"))
+
+print("\nsum of {0} and {1} is {2}".format(a,b,a+b))
+
+print("\ndiffrence of {0} and {1} is {2}".format(a,b,a-b))
+
+print("\nproduct of {0} and {1} is {2}".format(a,b,a*b))
+
+print("\ndivision  of {0} and {1} is {2}".format(a,b,a/b))
+
+print("\nremainder of {0} and {1} is {2}".format(a,b,a//b))
+
