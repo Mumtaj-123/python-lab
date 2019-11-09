51675b7c8db213412b074090f59fbfe043737622
Mumtaj-123 <57558099+Mumtaj-123@users.noreply.github.com>
2019-11-10 00:01:11 +0530

    Add files via upload

---
+++ swaping_two_number.py
@@ -0,0 +1,12 @@
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+
+print("Before the swapping a is " ,a)
+print("Before the swapping b is ",b)
+
+temp=a
+a=b
+b=temp
+
+print("After swapping a={0}".format(a))
+print("After swapping b={0}".format(b))
